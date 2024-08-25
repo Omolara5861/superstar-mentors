80ffffb49b884a3f064da39dadd91c3f70780a51
Omolara5861 <debbiegterra@gmail.com>
2024-08-25 02:03:16 +0100

    feat: :sparkles: Create reusuable button

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+interface ButtonProps {
+    btnText: React.ReactNode
+    href: string;
+    width?: string;
+}
+
+const Button: React.FC<ButtonProps> = ({ btnText, href, width }) => {
+    return (
+        <Link
+            href={href}
+        >
+            <button
+                style={{ width: width || 'auto' }}
+                className="inline-block bg-primary sm:w-auto py-3 px-4 rounded-lg text-white">
+                {btnText}
+            </button>
+        </Link>
+    );
+};
+
+export default Button;
