67a8bb3cd74b96b26a03fdeeaed495d9e34b1cfa
Omolara5861 <debbiegterra@gmail.com>
2024-08-26 23:22:03 +0100

    style: :lipstick: Change font family

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, DM_Sans } from "next/font/google";
 import "./globals.css";
 import Footer from "../components/footer/Footer";
 import Navbar from "../components/navbar/Navbar";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Superstar Mentors",
