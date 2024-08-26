9fb78b18e5de283dad7d84d1b3359b3b442f036e
Omolara5861 <debbiegterra@gmail.com>
2024-08-26 19:38:05 +0100

    feat: :sparkles: Create Homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import HomePage from "../pages/home/Home";
 
 export default function Home() {
   return (
     <>
-    
+    <HomePage />
     </>
   );
 }
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,44 @@
+"use client"
+import HeroSection from "../../components/home/hero/Hero";
+import MentorGrid from "../../components/home/mentors/Mentors";
+import MentorshipDetails from "../../components/home/mentorshipDetails/Mentorship";
+import TestimonialCarousel from "../../components/testimonial/Testimonial";
+import NewsLetter from "../../components/newsletter/NewsLetter";
+import Image from 'next/image';
+
+
+const HomePage = () => {
+    return (
+        <>
+            <HeroSection />
+            <MentorshipDetails />
+            <MentorGrid />
+            <TestimonialCarousel />
+            <div className="container m-auto flex flex-col justify-center py-10 text-center bg-white text-zinc-950">
+                <div className="flex flex-col w-full max-md:max-w-full">
+                    <div className="flex flex-col items-center w-full max-md:max-w-full">
+                        <div className="text-4xl font-bold tracking-tight leading-snug max-md:max-w-full">
+                            “STUDY FROM THE MASTER AND BE A MASTER YOURSELF”
+                        </div>
+                        <div className="mt-4 text-lg tracking-normal leading-6 w-[593px] max-md:max-w-full text-subtext">
+                                Our website helps you easily{" "}
+                            <span className="font-medium text-primary">IDENTIFY</span>
+                       ,
+                            <span className="font-medium text-primary">NETWORK,</span>{" "}
+                            <span className="font-medium text-primary">LEARN, MENTOR,</span>
+                             and 
+                            <span className="font-medium text-primary">GROW</span>
+                            
+                                your skills from any place.
+                        </div>
+                    </div>
+                
+                    <Image src='/demo.gif' alt='Demo Gif' width={1000} height={1000} className="w-full" />
+                </div>
+            </div>
+            <NewsLetter />
+        </>
+    );
+};
+
+export default HomePage;
