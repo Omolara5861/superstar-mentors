edd4de504dcc5ae8d457549c8ab8cdd27a117e99
Omolara5861 <debbiegterra@gmail.com>
2024-09-01 00:34:33 +0100

    feat: :sparkles: Update hero background image

--- src/pages/about/components/Hero.tsx
+++ src/pages/about/components/Hero.tsx
@@ -3,24 +3,27 @@ import Image from 'next/image';
 const HeroSection = () => {
     return (
         <>
-        <section className="relative bg-custom-gradient text-white py-20">
-            {/* Background Image */}
-            <div className="absolute inset-0">
-                <Image
-                    src="/about/about-bg.jpg" // Make sure to replace with your actual image path
-                    alt="Background image"
-                    layout="fill"
-                    objectFit="cover"
-                    className="opacity-50"
-                />
-            </div>
+            <section className="relative text-white py-20">
+                {/* Background Image */}
+                <div className="absolute inset-0">
+                    <Image
+                        src="/about/hero.png"
+                        alt="Background image"
+                        layout="fill"
+                        objectFit="cover"
+                        className="object-cover"
+                    />
+                </div>
 
-            {/* Content */}
-            <div className="relative z-10 flex flex-col items-center justify-center h-full text-center">
-                <h1 className="text-3xl md:text-4xl font-bold mb-4 text-white">About Us</h1>
-                <p className="text-lg md:text-xl">Your Reliable Hub For Mentorship</p>
-            </div>
-        </section>
+                {/* Gradient Overlay */}
+                <div className="absolute inset-0 bg-custom-gradient opacity-80"></div>
+
+                {/* Content */}
+                <div className="relative z-10 flex flex-col items-center justify-center h-full text-center">
+                    <h1 className="text-3xl md:text-4xl font-bold mb-4 text-white">About Us</h1>
+                    <p className="text-lg md:text-xl">Your Reliable Hub For Mentorship</p>
+                </div>
+            </section>
             <Image
                 alt="Decoration Image 2"
                 width={1440}
@@ -29,6 +32,7 @@ const HeroSection = () => {
                 className="object-contain w-full"
             />
         </>
+
     );
 };
 
