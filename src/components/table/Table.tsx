eff123b2619be7f3355a1b03db6ee82accc1458b
Omolara5861 <debbiegterra@gmail.com>
2024-12-24 14:15:30 +0100

    Add TAbles

---
+++ src/components/table/Table.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+type Column<T> = {
+    header: string;
+    accessor: keyof T;
+    render?: (value: T[keyof T], row: T) => React.ReactNode; // Custom render function
+};
+
+type TableProps<T> = {
+    data: T[];
+    columns: Column<T>[];
+};
+
+function Table<T>({ data, columns }: TableProps<T>) {
+    return (
+        <div className="overflow-x-auto">
+            <table className="min-w-full bg-white border border-gray-200">
+                <thead>
+                    <tr className="bg-gray-100">
+                        {columns.map((column, index) => (
+                            <th
+                                key={index}
+                                className="text-left px-6 py-3 text-sm font-semibold text-gray-700 border-b"
+                            >
+                                {column.header}
+                            </th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((row, rowIndex) => (
+                        <tr key={rowIndex} className="hover:bg-gray-50">
+                            {columns.map((column: any, colIndex: any) => (
+                                <td
+                                    key={colIndex}
+                                    className="px-6 py-4 text-sm text-gray-600 border-b"
+                                >
+                                    {column.render
+                                        ? column.render(row[column.accessor], row)
+                                        : row[column.accessor]!}
+                                </td>
+                            ))}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default Table;
