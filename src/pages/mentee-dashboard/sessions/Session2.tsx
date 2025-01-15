215960c84f92d08cc5534724be1411d382daecb3
Omolara5861 <debbiegterra@gmail.com>
2025-01-15 03:30:40 +0100

    Create Session

---
+++ src/pages/mentee-dashboard/sessions/Session2.tsx
@@ -0,0 +1,167 @@
+"use client"
+import Image from "next/image";
+import React, { useState } from "react";
+
+const SessionsDetails = () => {
+    const [activeTab, setActiveTab] = useState("Overview");
+    const [expandedTopics, setExpandedTopics] = useState<{ [key: string]: boolean }>({});
+    const [isExpandedAll, setIsExpandedAll] = useState(false);
+
+    const topics = [
+        { id: 1, title: "Introduction to Karate", details: "Learn the basics of Karate." },
+        { id: 2, title: "What is the following topic all about", details: "Explore the topic in detail." },
+        { id: 3, title: "Advanced Karate Techniques", details: "Master advanced Karate skills." },
+        { id: 4, title: "History of Karate", details: "Discover the history behind Karate." },
+        // Add more topics as needed
+    ];
+
+    const toggleExpandAll = () => {
+        const newExpandedState = !isExpandedAll;
+        setExpandedTopics(
+            topics.reduce((acc, topic) => {
+                acc[topic.id] = newExpandedState;
+                return acc;
+            }, {} as { [key: string]: boolean })
+        );
+        setIsExpandedAll(newExpandedState);
+    };
+
+    const toggleTopic = (id: number) => {
+        setExpandedTopics((prevState) => ({
+            ...prevState,
+            [id]: !prevState[id],
+        }));
+    };
+
+    return (
+        <div className="flex">
+
+            {/* Main Content */}
+            <div className="flex-1 p-6">
+                {/* Header */}
+                <h3 className="text-2xl font-medium text-[#555555]">Sessions</h3>
+                <div className=" text-[#555555] mb-8">
+                    Dashboard → Sessions
+                </div>
+
+                <div className="bg-white border-[0.5px] border-[#c4c2c2] p-6 rounded-[20px]">
+                    {/* Video Player Section */}
+                    <div>
+                        <p className="font-medium text-subtext mb-4">
+                            Introduction to Videography, 28 Nov 2024
+                        </p>
+                        <div className="h-[310px] bg-subtext rounded-lg mb-4"></div>
+                        <div className="flex justify-center space-x-4 mt-[-70px]">
+                            <Image src='/dashboard/audio.svg' alt="Audio" width={40} height={40} />
+                            <Image src='/dashboard/video.svg' alt="Audio" width={40} height={40} />
+                            <Image src='/dashboard/mute.svg' alt="Audio" width={40} height={40} />
+                            <Image src='/dashboard/end.svg' alt="Audio" width={40} height={40} />
+                            <Image src='/dashboard/download.svg' alt="Audio" width={40} height={40} />
+                            <Image src='/dashboard/members.svg' alt="Audio" width={40} height={40} />
+                        </div>
+                        <h3 className="text-2xl font-medium text-text mt-8">Getting started with Music</h3>
+                        <div className="flex gap-2 my-2">
+                            <Image src='/dashboard/img.svg' alt="Course" width={50} height={50} />
+                            <div>
+                                <p className="text-text text-lg font-medium">Eze Babatunde</p>
+                                <p className="text-subtext">
+                                    Music Mentor
+                                </p>
+                            </div>
+                        </div>
+
+                        {/* Tabs */}
+                        <div className="flex border-[0.5px] border-[#FFC145] bg-[#f5f5f5] p-2 mb-4 rounded-full lg:w-1/3 w-full">
+                            {["Overview", "Resources", "Description"].map((tab) => (
+                                <button
+                                    key={tab}
+                                    onClick={() => setActiveTab(tab)}
+                                    className={`px-4 py-2 text-sm font-medium ${activeTab === tab
+                                        ? " text-primary"
+                                        : "text-gray-600 hover:text-green-600"
+                                        }`}
+                                >
+                                    {tab}
+                                </button>
+                            ))}
+                        </div>
+
+                        {/* Tab Content */}
+                        <div>
+                            {activeTab === "Overview" && (
+                                <p className="text-sm text-gray-600">
+                                    Lorem ipsum dolor sit amet consectetur. Tellus cursus natoque
+                                    aenean tortor. Non ac blandit elit pulvinar vulputate lorem ac
+                                    sed.
+                                    Lorem ipsum dolor sit amet consectetur. Tellus cursus natoque aenean tortor. Non ac blandit elit pulvinar vulputate lorem ac sed.
+                                    Lorem ipsum dolor sit amet consectetur. Tellus cursus natoque aenean tortor. Non ac blandit elit pulvinar vulputate lorem ac sed. 
+                                </p>
+                            )}
+                            {activeTab === "Resources" && (
+                                <p className="text-sm text-gray-600">
+                                    Resources for this session will be listed here.
+                                </p>
+                            )}
+                            {activeTab === "Description" && (
+                                <p className="text-sm text-gray-600">
+                                    A detailed description of the session will go here.
+                                </p>
+                            )}
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* Sidebar */}
+            <div className="w-72 ">
+                {/* Learning Reminder */}
+                <div className="bg-[#19470A] text-[#f5f5f5] p-4 rounded-lg mb-6 shadow-[0px_2px_2px_0px_#0000000D] text-xs">
+                    <p className="uppercase font-medium mb-3">Learning Reminder</p>
+                    <p className="text-sm mb-1">
+                        Set Your Learning Time
+                    </p>
+                    <p>
+                        Set your learning time as comfortable as you want
+                    </p>
+                </div>
+
+                {/* Course Topics */}
+                <div className="w-72 bg-white border-[0.5px] border-[#c4c2c2] p-4 rounded-lg">
+                    {/* Course Topics */}
+                    <div>
+                        <div className="flex justify-between items-center mb-4">
+                            <h3 className="text-sm font-medium text-subtext">Course</h3>
+                            <button
+                                onClick={toggleExpandAll}
+                                className="text-sm text-[#D3900A] underline"
+                            >
+                                {isExpandedAll ? "Collapse All" : "Expand All"}
+                            </button>
+                        </div>
+                        <ul className="space-y-2">
+                            {topics.map((topic) => (
+                                <li key={topic.id} className="pb-2">
+                                    <div
+                                        onClick={() => toggleTopic(topic.id)}
+                                        className="flex gap-2 items-center cursor-pointer hover:bg-gray-100 p-2 rounded-lg bg-[#f5f5f5] border-[0.5] border-[#f5f5f5]"
+                                    >
+                                        <Image src='/dashboard/bulb.svg' alt="Bulb" width={30} height={30} />
+                                        <span className="text-[15px] text-text">{topic.title}</span>
+                                    </div>
+                                    {expandedTopics[topic.id] && (
+                                        <div className="mt-2 text-sm text-gray-600 ml-6 flex gap-2 items-center">
+                                            <Image src='/dashboard/open-book.svg' alt="Bulb" width={30} height={30} />
+                                            {topic.details}
+                                        </div>
+                                    )}
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SessionsDetails;
--- src/pages/mentee-dashboard/sessions/Sessions.tsx
+++ src/pages/mentee-dashboard/sessions/Sessions.tsx
@@ -1,11 +1,23 @@
+"use client"
 import Image from 'next/image';
 import Wrapper from '../../../components/mentee-dashboard/wrapper/Wrapper';
 import Link from 'next/link';
-
+import { useState } from 'react';
+import SessionDetails from './Session2';
 
 function SessionsPage() {
+    const [sessionActive, setSessionActive] = useState(false);
+
     return (
         <main className="flex-1 mt-3">
+
+            {/* Conditional rendering based on sessionActive state */}
+            {sessionActive ? (
+                <SessionDetails />
+            ) : (
+                    <>
+                    
+
             {/* Current Session Header */}
             <div className="bg-white shadow-[0px_2px_4px_0px_#00000014] rounded-lg p-4 flex items-center justify-between mb-6 border-[0.5px] border-primary">
                 <div className="flex gap-2">
@@ -17,88 +29,90 @@ function SessionsPage() {
                         </p>
                     </div>
                 </div>
-                <button className="px-6 py-2 bg-primary text-white rounded-lg shadow hover:bg-green-700">
+                <button className="px-6 py-2 bg-primary text-white rounded-lg shadow hover:bg-green-700" onClick={() => setSessionActive(true)}>
                     Join Session
                 </button>
             </div>
-
-            <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
-                {/* Sessions Grid */}
-                <div className="lg:col-span-2 grid grid-cols-1 sm:grid-cols-3 gap-4">
-                    {Array(6)
-                        .fill(0)
-                        .map((_, index) => (
-                            <div
-                                key={index}
-                                className="bg-white border-[0.5px] border-[#6D6D6D38] rounded-[10px] p-4 shadow-[0px_2px_4px_0px_#0000000D]"
-                            >
-                                <div className="h-40 bg-[#808182] rounded mb-4"></div>
-                                <p className="text-sm font-bold mb-2 text-text">
-                                    Course name-Headline for a course
-                                </p>
-                                <div className="flex gap-2 my-2">
-                                    <Image src='/dashboard/img.svg' alt="Course" width={50} height={50} />
-                                    <div className='text-subtext '>
-                                        <p className="text-sm">Rose Effiong</p>
-                                        <p className="text-xs">
-                                            Roseeffi@gmail.com
-                                        </p>
-                                    </div>
-                                </div>
-                                <Link href='/' className="text-subtext font-medium underline">View Now</Link>
-                            </div>
-                        ))}
-                </div>
-
-                {/* Sidebar: Previous Records and Upcoming Sessions */}
-                <div className="lg:col-span-1 space-y-6">
-                    {/* Previous Class Records */}
-                    <div className='bg-white p-[10px] rounded-lg'>
-                        <p className="font-medium text-center text-subtext mb-2">Previous Class Records</p>
-                        <div className="space-y-3">
-                            {["Steps in Videography", "How to Videograph your work", "How to handle your camera"].map(
-                                (title, index) => (
-                                    <div key={index} className="flex items-center justify-between p-[10px] rounded-[10px] bg-white border-[0.5px] border-[#FFC145]">
-                                        <div>
-                                            <p className="text-sm text-text font-medium">
-                                                {title}
+                <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+                    {/* Sessions Grid */}
+                    <div className="lg:col-span-2 grid grid-cols-1 sm:grid-cols-3 gap-4">
+                        {Array(6)
+                            .fill(0)
+                            .map((_, index) => (
+                                <div
+                                    key={index}
+                                    className="bg-white border-[0.5px] border-[#6D6D6D38] rounded-[10px] p-4 shadow-[0px_2px_4px_0px_#0000000D]"
+                                >
+                                    <div className="h-40 bg-[#808182] rounded mb-4"></div>
+                                    <p className="text-sm font-bold mb-2 text-text">
+                                        Course name-Headline for a course
+                                    </p>
+                                    <div className="flex gap-2 my-2">
+                                        <Image src='/dashboard/img.svg' alt="Course" width={50} height={50} />
+                                        <div className='text-subtext '>
+                                            <p className="text-sm">Rose Effiong</p>
+                                            <p className="text-xs">
+                                                Roseeffi@gmail.com
                                             </p>
-                                            <p className='text-subtext text-sm my-2'>Theory</p>
-                                            <div className="flex gap-2 items-center text-xs text-subtext">
-                                                <Image src="/dashboard/calendar.svg" width={24} height={24} alt="Calendar" />
-                                                <span>22/08/2024</span>
-                                            </div>
                                         </div>
-                                        <button >
-                                            <Image src="/dashboard/play.svg" width={24} height={24} alt="Play" />
-                                        </button>
                                     </div>
-                                )
-                            )}
-                        </div>
+                                    <Link href='/' className="text-subtext font-medium underline">View Now</Link>
+                                </div>
+                            ))}
                     </div>
 
-                    {/* Upcoming Sessions */}
-                    <div className="bg-white p-[18px] rounded-lg">
-                        <p className="font-medium text-center text-subtext mb-2">Upcoming Sessions</p>
-                        <div className="space-y-3">
-                            {["Mentorship session with Omowunmi Dada", "Practical session in videography", "Practical session in videography", "Practical session in videography"].map(
-                                (title, index) => (
-                                    <div
-                                        key={index}
-                                        className={`flex justify-between rounded-[4px] p-[13px] text-sm rounded-s
-                                        shadow-[0px_4px_4px_0px_#00000014] ${index === 0 ? 'bg-gradient-to-r from-[#006A50] to-[#FFC145] text-[#F1FFFB]' : 'bg-white text-text'}`}
-                                    >
-                                        <p className="font-medium max-w-[50%]">{title}</p>
-                                        <p>9AM - 10AM <br /> 22/08/2024</p>
-                                    </div>
-                                )
-                            )}
+                    {/* Sidebar: Previous Records and Upcoming Sessions */}
+                    <div className="lg:col-span-1 space-y-6">
+                        {/* Previous Class Records */}
+                        <div className='bg-white p-[10px] rounded-lg'>
+                            <p className="font-medium text-center text-subtext mb-2">Previous Class Records</p>
+                            <div className="space-y-3">
+                                {["Steps in Videography", "How to Videograph your work", "How to handle your camera"].map(
+                                    (title, index) => (
+                                        <div key={index} className="flex items-center justify-between p-[10px] rounded-[10px] bg-white border-[0.5px] border-[#FFC145]">
+                                            <div>
+                                                <p className="text-sm text-text font-medium">
+                                                    {title}
+                                                </p>
+                                                <p className='text-subtext text-sm my-2'>Theory</p>
+                                                <div className="flex gap-2 items-center text-xs text-subtext">
+                                                    <Image src="/dashboard/calendar.svg" width={24} height={24} alt="Calendar" />
+                                                    <span>22/08/2024</span>
+                                                </div>
+                                            </div>
+                                            <button >
+                                                <Image src="/dashboard/play.svg" width={24} height={24} alt="Play" />
+                                            </button>
+                                        </div>
+                                    )
+                                )}
+                            </div>
+                        </div>
+
+                        {/* Upcoming Sessions */}
+                        <div className="bg-white p-[18px] rounded-lg">
+                            <p className="font-medium text-center text-subtext mb-2">Upcoming Sessions</p>
+                            <div className="space-y-3">
+                                {["Mentorship session with Omowunmi Dada", "Practical session in videography", "Practical session in videography", "Practical session in videography"].map(
+                                    (title, index) => (
+                                        <div
+                                            key={index}
+                                            className={`flex justify-between rounded-[4px] p-[13px] text-sm rounded-s
+                                            shadow-[0px_4px_4px_0px_#00000014] ${index === 0 ? 'bg-gradient-to-r from-[#006A50] to-[#FFC145] text-[#F1FFFB]' : 'bg-white text-text'}`}
+                                        >
+                                            <p className="font-medium max-w-[50%]">{title}</p>
+                                            <p>9AM - 10AM <br /> 22/08/2024</p>
+                                        </div>
+                                    )
+                                )}
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
+                        </div>
+                    </>
+            )}
         </main>
     );
 }
+
 export default Wrapper(SessionsPage);
