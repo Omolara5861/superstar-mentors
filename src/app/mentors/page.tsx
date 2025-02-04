d959b9bd1c3393a90d2f1214e8c0c8094ced1750
Omolara5861 <debbiegterra@gmail.com>
2025-02-04 13:29:35 +0100

    fix: :bug: Fix suspense bug

--- src/app/mentors/page.tsx
+++ src/app/mentors/page.tsx
@@ -1,13 +1,14 @@
+import { Suspense } from "react";
 import Footer from "../../components/footer/Footer";
 import Navbar from "../../components/navbar/Navbar";
 import MentorsPage from "../../pages/mentors/mentors";
 
 export default function About() {
     return (
-        <>
+        <Suspense fallback={<div>Loading mentors...</div>}>
             <Navbar />
             <MentorsPage />
             <Footer />
-        </>
+        </Suspense>
     );
 }
